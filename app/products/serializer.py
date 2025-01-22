37c7f56672d7f5e27a9c53e09f6efefee86248b0
Atilio <atilio.jesus@grupoboticario.com.br>
2025-01-22 18:01:48 -0300

    🐛 remove codigo antigo

--- app/products/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import ProductModel
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProductModel
-        fields = '__all__'
